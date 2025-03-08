cd8ba6a3b5304830ff802da03817c4fe4958604f
kswarj <kswaroop74@gmail.com>
2025-03-07 22:59:49 -0500

    Added problem statement as a doc string, changed the output print statement after rolling the die

--- Roll_Dice/src/roll_dice_game.py
+++ Roll_Dice/src/roll_dice_game.py
@@ -1,6 +1,6 @@
-#Rolling dice game with user inputs 
-
-
+"""Write a program that simulates  rolling  a pair  of dice . Each time the program runs,it should randomly generate 
+two numbers between 1 and 6 (inclusive), representing the result of each die.THr program should then display  the results and ask if you 
+want to roll again"""
 import random
 
 # function to roll dice
@@ -15,7 +15,7 @@ def roll_dice():
         if user_input == 'y':
             die1 = random.randint(1,6)
             die2 = random.randint(1,6)
-            print(f'({die1},{die2})')
+            print(f"You rolled  a {die1} and a {die2}")
         elif (user_input == 'n'):
             print("Thanks for playing the game. Have a great day!!!")
             break
